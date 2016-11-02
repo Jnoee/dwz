e276c491cfbd9915e22935a08a955358a9bfe5c3
jnoee <jnoee.he@gmail.com>
2016-11-02 18:13:24 +0800

    d

--- src/dwz.webuploader.js
+++ src/dwz.webuploader.js
@@ -29,8 +29,8 @@
         server: 'result/upload-success.json',
         accept: {
           title: 'Images',
-          extensions: 'gif,jpg,jpeg,bmp,png',
-          mimeTypes: 'image/gif,image/jpg,image/jpeg,image/bmp,image/png'
+          extensions: 'gif,jpg,jpeg,png',
+          mimeTypes: 'image/gif,image/jpg,image/jpeg,image/png'
         },
         fileSingleSizeLimit: 1 * 1024 * 1024,
         duplicate: true,
